db7b75f9b167973f90d1103e6cfaca625fa1fec2
Qubus0 <steen.rickmer@gmx.de>
2023-06-06 16:27:53 +0200

    initial directory configs

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+DIR_DATA = 'data'
+DIR_TRAINING = os.path.join(DIR_DATA, 'training')
+DIR_VALIDATION = os.path.join(DIR_DATA, 'validation')
+DIR_TEST = os.path.join(DIR_DATA, 'test')
+DIR_DISCARD = os.path.join(DIR_DATA, 'discard')
+
+
---
+++ src/main.py
@@ -0,0 +1,6 @@
+import config
+
+
+
+if __name__ == '__main__':
+	print(config.DIR_DATA)
