60d33ff02f5e3ac6301f63e89eddf7de705f254f
Qubus0 <steen.rickmer@gmx.de>
2023-06-06 17:50:59 +0200

    labelbox annotation export

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ import config
 from dotenv import load_dotenv
 from pathlib import Path
 
-
+import src.labelbox_annotations as labelbox_annotations
 
 if __name__ == '__main__':
 	dotenv_path = Path('../.env.local')
@@ -13,5 +13,10 @@ if __name__ == '__main__':
 	if not os.getenv('LABELBOX_API_KEY'):
 		print('LABELBOX_API_KEY not set. Create a .env.local file in the root directory and set the key there.')
 		exit(1)
+
+	if not os.path.exists(config.DIR_CURRENT_DATASET):
+		os.makedirs(config.DIR_CURRENT_DATASET)
+
+	labelbox_annotations.download_json(os.getenv('LABELBOX_API_KEY'))
 
 	print(config.DIR_DATA)
